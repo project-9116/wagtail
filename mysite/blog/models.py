36d01d9ed73245a9fa8147e9879786a492e4b086
Volkan TAŞ <djangoogreniyorum@gmail.com>
2024-11-26 19:32:10 +0300

    Tag bölümü eklendi.

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,8 @@ from django import forms
 from django.db import models
 
 from modelcluster.fields import ParentalKey, ParentalManyToManyField
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from taggit.models import TaggedItemBase
 
 from wagtail.models import Page, Orderable
 from wagtail.fields import RichTextField
@@ -10,12 +12,10 @@ from wagtail.snippets.models import register_snippet
 
 from wagtail.search import index
 
-# keep the definition of BlogIndexPage model, and add the BlogPage model:
 class BlogIndexPage(Page):
     intro = RichTextField(blank=True)
 
     def get_context(self, request):
-        # Update context to include only published posts, ordered by reverse-chron
         context = super().get_context(request)
         blogpages = self.get_children().live().order_by('-first_published_at')
         context['blogpages'] = blogpages
@@ -25,12 +25,21 @@ class BlogIndexPage(Page):
         FieldPanel('intro')
     ]
 
+class BlogPageTag(TaggedItemBase):
+    content_object = ParentalKey(
+        'BlogPage',
+        related_name='tagged_items',
+        on_delete=models.CASCADE
+    )
+
+
 class BlogPage(Page):
     date = models.DateField("Post date")
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
-
     authors = ParentalManyToManyField('blog.Author', blank=True)
+
+    tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
 
     def main_image(self):
         gallery_item = self.gallery_images.first()
@@ -44,10 +53,12 @@ class BlogPage(Page):
         index.SearchField('body'),
     ]
 
+    
     content_panels = Page.content_panels + [
         MultiFieldPanel([
             FieldPanel('date'),
             FieldPanel('authors', widget=forms.CheckboxSelectMultiple),
+            FieldPanel('tags'),
         ], heading="Blog information"),
         FieldPanel('intro'),
         FieldPanel('body'),
@@ -85,3 +96,16 @@ class Author(models.Model):
 
     class Meta:
         verbose_name_plural = 'Authors'
+
+class BlogTagIndexPage(Page):
+
+    def get_context(self, request):
+
+        # Filter by tag
+        tag = request.GET.get('tag')
+        blogpages = BlogPage.objects.filter(tags__name=tag)
+
+        # Update template context
+        context = super().get_context(request)
+        context['blogpages'] = blogpages
+        return context
